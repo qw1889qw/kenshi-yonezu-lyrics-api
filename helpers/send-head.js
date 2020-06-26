2808eb161728de7fb3c3c479b8626aaa077f788e
questionedandmarked <qw1889qw@gmail.com>
2020-06-26 15:42:26 -0700

    create help file

--- helpers/send-head.js
+++ helpers/send-head.js
@@ -8,6 +8,7 @@ const sendHead = (stream, worked) => {
   stream.writeHead(statusCode, {
     'Content-Type': 'text/plain;charset=utf-8'
   });
+  // this part doesn't exactly fit the function name unfortunately
   if (statusCode === 404) {
     stream.end('not found/見つかりませんでした');
   }
